99fa2a6fa31087d9ef9cb75e1f8373d091cc3ac3
Gavin Eke <GavinEke@users.noreply.github.com>
2016-01-19 09:18:18 +1100

    Update Remove-GWX.ps1

--- Remove-GWX.ps1
+++ Remove-GWX.ps1
@@ -2,5 +2,5 @@
 Stop-Process -Name GWX, GWXUX -Force
 # Disable GWX in Registry
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Gwx" -Name DisableGwx -Value 1 -Force
-# Uninstall KB3035583
-wusa /uninstall /kb:3035583 /quiet /norestart
+# Disable OS Upgrade in Registry
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\WindowsUpdate" -Name DisableOSUpgrade -Value 1 -Force
