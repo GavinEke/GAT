6cf7943a62ae707ca7ff517d677f5dbb6b9dfca4
Gavin Eke <GavinEke@users.noreply.github.com>
2016-08-05 04:14:53 +1000

    Delete Remove-Win8StoreApps.ps1

--- Remove-Win8StoreApps.ps1
+++
@@ -1,3 +0,0 @@
-# DEPRECATED: Replace with Remove-WinAppxBloat.ps1 - https://github.com/GavinEke/GAT/blob/master/Remove-WinAppxBloat.ps1
-Get-AppxProvisionedPackage -online | Remove-AppxProvisionedPackage -online
-Get-AppxPackage -AllUsers | Remove-AppxPackage
