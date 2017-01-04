0febb123a486ed77f4fe1f2a26e643f4365a2362
Gavin Eke <GavinEke@users.noreply.github.com>
2017-01-04 12:38:47 +1100

    Changed to Module

---
+++ GAT/Functions/Enable-RDP.ps1
@@ -0,0 +1,7 @@
+﻿Function Enable-RDP {
+    # Allow Remote Desktop in Firewall
+    Set-NetFirewallRule -DisplayGroup “Remote Desktop” –Enabled True
+    # Enable Remote Desktop in Registry
+    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name UserAuthentication -Value 0 -Force
+    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name fDenyTSConnections -Value 0 -Force
+}
---
+++ GAT/GAT.psm1
@@ -0,0 +1,11 @@
+﻿#Requires -RunAsAdministrator
+
+# Set PSScriptRoot
+If (!($PSScriptRoot)) {
+    $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+}
+
+# Dot source the modules functions
+$CFFunctions = Get-ChildItem -Path "$PSScriptRoot\Functions\*.ps1"
+$CFFunctions | ForEach-Object {. $_.FullName}
+Export-ModuleMember -Function $CFFunctions.BaseName
