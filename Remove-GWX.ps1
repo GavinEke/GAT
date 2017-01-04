3fb1d531b1198b3a34ec0a5b126a6d42e5444576
Gavin Eke <GavinEke@users.noreply.github.com>
2017-01-04 12:40:09 +1100

    Delete Remove-GWX.ps1

--- Remove-GWX.ps1
+++
@@ -1,6 +0,0 @@
-﻿# Stop GWX Processes
-Stop-Process -Name GWX, GWXUX -Force
-# Disable GWX in Registry
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Gwx" -Name DisableGwx -Value 1 -Force
-# Disable OS Upgrade in Registry
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\WindowsUpdate" -Name DisableOSUpgrade -Value 1 -Force
