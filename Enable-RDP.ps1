9a3d7512ffb19d18a852bf49df538e8cc959dc7f
Gavin Eke <GavinEke@users.noreply.github.com>
2017-01-04 12:39:40 +1100

    Delete Enable-RDP.ps1

--- Enable-RDP.ps1
+++
@@ -1,5 +0,0 @@
-﻿# Allow Remote Desktop in Firewall
-Set-NetFirewallRule -DisplayGroup “Remote Desktop” –Enabled True
-# Enable Remote Desktop in Registry
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name UserAuthentication -Value 0 -Force
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name fDenyTSConnections -Value 0 -Force
