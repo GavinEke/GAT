0d5c6c3a5dd510058d840645725e8731d0912a76
Gavin Eke <GavinEke@users.noreply.github.com>
2017-01-04 12:39:12 +1100

    Delete Disable-Win10Telemetry.ps1

--- Disable-Win10Telemetry.ps1
+++
@@ -1,4 +0,0 @@
-# Disable CEIP Tasks
-Get-ScheduledTask -TaskPath "\Microsoft\Windows\Customer Experience Improvement Program\" | Disable-ScheduledTask
-# Only send security related telemetry [Enterprise] or basic [All other editions]
-New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name AllowTelemetry -Value 0 -Force
