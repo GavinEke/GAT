963fba6d32277486c4e15149bd7e06be0005e2a2
Gavin Eke <GavinEke@users.noreply.github.com>
2018-02-12 10:49:39 +1100

    transform string to literal string

--- GAT/Functions/Disable-WindowsTelemetry.ps1
+++ GAT/Functions/Disable-WindowsTelemetry.ps1
@@ -1,6 +1,6 @@
 Function Disable-WindowsTelemetry {
     # Disable CEIP Tasks
-    Get-ScheduledTask -TaskPath "\Microsoft\Windows\Customer Experience Improvement Program\" | Disable-ScheduledTask
+    Get-ScheduledTask -TaskPath '\Microsoft\Windows\Customer Experience Improvement Program\' | Disable-ScheduledTask
     # Only send security related telemetry [Enterprise] or basic [All other editions]
-    New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name AllowTelemetry -Value 0 -Force
+    New-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\DataCollection' -Name AllowTelemetry -Value 0 -Force
 }
