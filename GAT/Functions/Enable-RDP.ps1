55475254f3fc94cf6dc860358d682c1cf64c6c76
Chris Gardner <Chris@blackmarble.com>
2018-07-29 21:39:39 +0100

    replace em dash with real dash

--- GAT/Functions/Enable-RDP.ps1
+++ GAT/Functions/Enable-RDP.ps1
@@ -1,6 +1,6 @@
 Function Enable-RDP {
     # Allow Remote Desktop in Firewall
-    Set-NetFirewallRule -DisplayGroup 'Remote Desktop' –Enabled True
+    Set-NetFirewallRule -DisplayGroup 'Remote Desktop' -Enabled True
     # Enable Remote Desktop in Registry
     New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name UserAuthentication -Value 0 -Force
     New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name fDenyTSConnections -Value 0 -Force
